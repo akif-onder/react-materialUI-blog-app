ecafa9c354d046b3f88cfdba4062f45b118a752d
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-08-03 21:21:39 +0300

    navbar completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,55 +1,111 @@
-import { Facebook, Instagram, Twitter } from "@mui/icons-material";
-import { AppBar, Box, InputBase, styled, Toolbar, Typography } from "@mui/material";
-import React from "react";
+import {
+  Facebook,
+  Instagram,
+  Menu as MenuIcon,
+  Twitter,
+} from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Grid,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+
+
 // import FacebookIcon from '@mui/icons-material/Facebook';
 // import InstagramIcon from '@mui/icons-material/Instagram';
 // import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Navbar = () => {
-    const StyledToolbar = styled(Toolbar)({
-        display: 'flex',
-        justifyContent: 'space-between', 
-    })
+  const StyledToolbar = styled(Toolbar)({
+    display: "flex",
+    justifyContent: "space-between",
+  });
 
-    const SocialBox = styled(Box)({
-        display: 'flex',
-        gap: 10,
+  const SocialBox = styled(Box)({
+    display: "flex",
+    gap: 10,
+  });
 
-    })
+  const MenuBox = styled(Box)({
+    display: "flex",
+    gap: 30,
+  });
+  const SearchBox = styled(Box)({
+    display: "flex",
+    gap: 5,
+  });
 
-    const MenuBox = styled(Box)({
-        display: 'flex',
-        gap: 30,
+  const MenuItems = [
+    { Name: "Home", Link: "/" },
+    { Name: "Products", Link: "#" },
+    { Name: "Portfolio", Link: "#" },
+    { Name: "Blog", Link: "#" },
+    { Name: "Contact Us", Link: "#" },
+  ];
 
-    })
-    const SearchBox = styled(Box)({
-        display: 'flex',
-        gap: 5,
-
-    })
-
-    const MenuItems = [
-        {Name: 'Home', Link: '/'},
-        {Name: 'Products', Link: '#'},
-        {Name: 'Portfolio', Link: '#'},
-        {Name: 'Blog', Link: '#'},
-        {Name: 'Contact Us', Link: '#'}
-    ]
+  const [open, setOpen] = useState(false);
   return (
-    <AppBar sx={{background:'black'}}>
+    <AppBar sx={{ background: "black" }}>
       <StyledToolbar>
         <SocialBox>
-            <Facebook/>
-            <Instagram/>
-            <Twitter/>
+          <Facebook />
+          <Instagram />
+          <Twitter />
         </SocialBox>
-        <MenuBox>
-            {MenuItems.map((item)=>(
-                <Typography sx={{cursor: 'pointer', fontSize:'14px'}}>{item.Name}</Typography>
-            ))}
+        <MenuBox sx={{ display: { xs: "none", sm: "none", md: "flex" } }}>
+          {MenuItems.map((item) => (
+            <Typography sx={{ cursor: "pointer", fontSize: "14px" }}>
+              {item.Name}
+            </Typography>
+          ))}
         </MenuBox>
-        <SearchBox><InputBase placeholder="Search..." sx={{color:'white'}}/></SearchBox>
+        <SearchBox>
+          <InputBase placeholder="Search..." sx={{ color: "white" }} />
+          <MenuIcon
+            sx={{
+              color: "white",
+              display: { xs: "block", sm: "block", md: "none" },
+            }}
+            onClick={()=>setOpen(!open)}
+          />
+        </SearchBox>
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={()=>setOpen(!open)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <Grid>
+          {MenuItems.map((item) => (
+            <MenuItem
+              sx={{
+                cursor: "pointer",
+                fontSize: "14px",
+                background: "lightblue",
+                mb: "2px",
+              }}
+            >
+              {item.Name}
+            </MenuItem>
+          ))}
+        </Grid>
+      </Menu>
     </AppBar>
   );
 };
